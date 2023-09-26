039b64c06db6932f05b90b7f87146700d5cf31ee
vladihka <vladihka58@gmail.com>
2023-09-26 18:28:39 +0200

    Adding a saving feedbacks to database

---
+++ app/api/feedback/route.js
@@ -0,0 +1,11 @@
+import { Feedback } from "@/app/models/Feedback";
+import mongoose from "mongoose";
+
+export async function POST(request){
+    const jsonBody = await request.json();
+    const {title, description} = jsonBody;
+    const mongoUrl = process.env.MONGO_URL;
+    mongoose.connect(mongoUrl);
+    await Feedback.create({title, description});
+    return Response.json(jsonBody);
+}
--- app/components/FeedbackFormPopup.js
+++ app/components/FeedbackFormPopup.js
@@ -1,17 +1,40 @@
+import { useState } from "react";
 import Button from "./Button";
 import Popup from "./Popup";
+import axios from "axios";
 
 export default function FeedbackFormPopup({setShow}){
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+
+    function handleCreatePostButtonClick(ev){
+        ev.preventDefault();
+        axios.post('/api/feedback', {title, description}).then(() => {
+            setShow(false);
+        });
+    }
+
     return (
         <Popup setShow={setShow} title={'Make a suggestion'}>
             <form className="p-8">
                 <label className="block mt-4 mb-1 text-slate-700">Title</label>
-                <input className="w-full border p-2 rounded-md" type="text" placeholder="A short, descriptive title"></input>
+                <input 
+                    value={title}
+                    className="w-full border p-2 rounded-md" 
+                    type="text" 
+                    placeholder="A short, descriptive title"
+                    onChange={ev => setTitle(ev.target.value)}>
+                </input>
                 <label className="block mt-4 mb-1 text-slate-700">Details</label>
-                <textarea className="w-full border p-2 rounded-md" placeholder="Please include any details"></textarea>
+                <textarea 
+                    className="w-full border p-2 rounded-md" 
+                    placeholder="Please include any details"
+                    value={description}
+                    onChange={ev => setDescription(ev.target.value)}>
+                    </textarea>
                 <div className="flex gap-2 mt-2 justify-end">
                     <Button>Attach files</Button>
-                    <Button primary>Create post</Button>
+                    <Button primary onClick={handleCreatePostButtonClick}>Create post</Button>
                 </div>
             </form>
         </Popup>
